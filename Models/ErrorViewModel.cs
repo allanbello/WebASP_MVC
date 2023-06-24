dba087b1063642095a573673a48559f7c8392beb
allanbello <allanbello_@hotmail.com>
2023-06-23 23:02:44 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebASP_CoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
